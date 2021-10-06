5cad377aa049fa2ebdf57896cfb6d3a295a58c5f
zutlh <liuhao19971212@163.com>
2021-10-06 18:41:23 +0800

    添加了上传头像功能

--- src/views/edu/teacher/save.vue
+++ src/views/edu/teacher/save.vue
@@ -24,6 +24,33 @@
         <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
       </el-form-item>
       <!-- 讲师头像:todo -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+ 
+          <!-- 头衔缩略图 -->
+          <pan-thumb :image="teacher.avatar"/>
+          <!-- 文件上传按钮 -->
+          <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+          </el-button>
+ 
+      <!--
+        v-show：是否显示上传组件
+        :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+        :url：后台上传的url地址
+        @close：关闭上传组件
+        @crop-upload-success：上传成功后的回调 
+          <input type="file" name="file"/>
+      -->
+          <image-cropper
+              v-show="imagecropperShow"
+              :width="300"
+              :height="300"
+              :key="imagecropperKey"
+              :url="BASE_API+'/eduoss/fileoss'"
+              field="file"
+              @close="close"
+              @crop-upload-success="cropSuccess"/>
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" :disabled="saveBtnDisabled" @click="saveOrUpdate">保存</el-button>
       </el-form-item>
@@ -33,8 +60,10 @@
 
 <script>
 import teacherApi from '@/api/edu/teacher.js'
-
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 export default {
+  components:{ImageCropper,PanThumb},
   data(){
     return{
       teacher:{
@@ -45,6 +74,9 @@ export default {
         intro:'',
         avatar:'' 
       },
+      imagecropperShow:false,
+      imagecropperKey:0, //上传组件key值
+      BASE_API:process.env.VUE_APP_BASE_API,//获取ip和端口号
       saveBtnDisabled:false // 保存按钮是否禁用
     }
   },
@@ -57,6 +89,15 @@ export default {
     }
   },
   methods:{
+    close(){ //关闭上传弹窗的方法
+      this.imagecropperShow=false
+    },
+    // 上传成功方法
+    cropSuccess(data){
+      this.imagecropperShow=false
+      //上传成功之后接口返回图片地址
+      this.teacher.avatar=data.url
+    },
     init(){
     //判断路径是否有id值
     if(this.$route.params && this.$route.params.id){
